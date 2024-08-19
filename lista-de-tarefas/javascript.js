7f9a82786d76831c7ead1104c53085051a7c3c18
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-08-19 09:30:22 -0300

    modificações

--- lista-de-tarefas/javascript.js
+++ lista-de-tarefas/javascript.js
@@ -1,19 +1,3 @@
-function horario() {
-    var data = new Date();
-    var hora = data.getHours()
-    var body = document.body
-
-    if (hora >= 6 && hora <= 12) {
-        body.style.background = '#306088';  // Manhã
-    } else if (hora > 12 && hora <= 15) {
-        body.style.background = '#efb979';  // Tarde
-    } else if (hora > 15 &&  hora <= 18) {
-        body.style.background = '#FF6600';
-    }else {
-        body.style.background = '#07072b';  // Noite
-    }
-}
-
 function adicionar() {
      let tarefa = document.getElementById('tarefa')
      let escolha = document.getElementsByName('opcao')  /*Alterei para getElementsByName para capturar os elementos corretamente*/
@@ -34,13 +18,13 @@ function adicionar() {
          return // Retorna para sair da função se nenhum nível de importância estiver selecionado
     }
     if(escolha[1].checked){
-         let item = document.createElement('option')
+         let item = document.createElement('div')
          item.textContent = tarefa.value
          item.style.color = 'red'
          lista.appendChild(item)
          escolha[1].checked = false
     }else if(escolha[0].checked){
-         let item = document.createElement('option')
+         let item = document.createElement('div')
          item.textContent = tarefa.value
          lista.appendChild(item)
          escolha[0].checked = false
