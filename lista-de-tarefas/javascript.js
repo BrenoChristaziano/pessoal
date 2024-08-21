0a77412257ee8a6f57c35cda2207c8d30470c295
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-08-21 10:39:30 -0300

    tentando criar o adicionar

--- lista-de-tarefas/javascript.js
+++ lista-de-tarefas/javascript.js
@@ -1,5 +1,5 @@
 const tarefa = document.querySelector('#tarefa')
-const norma = document.querySelector('#normal')
+const normal = document.querySelector('#normal')
 const importante = document.querySelector('#importante')
 const btnadicionar = document.querySelector('#adicionar')
 const btnconcluido = document.querySelector('#concluido')
@@ -7,6 +7,8 @@ const btnpendente = document.querySelector('#pendente')
 const btnremover = document.querySelector('#remover')
 const caixa = document.querySelector('#caixaRolagem')
 
-const uma_nova_tarefa =(tarefa)=>{
-    
-}
+
+
+
+
+
